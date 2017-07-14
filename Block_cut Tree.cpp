b50590117880cb922e763bdf07a089d6202617d5
Munawar Hussain Shakil <shakil.ruet.12@gmail.com>
2017-07-15 02:23:44 +0600

    Create Block_cut Tree.cpp

---
+++ Block_cut Tree.cpp
@@ -0,0 +1,110 @@
+vector< vi > E;
+vector< vi > C;
+vector< vi > G;
+vector< vi > D;
+bool artp[MAXN];
+int artn[MAXN];
+vi realArt;
+int dfsnum[MAXN],dfslo[MAXN],par[MAXN];
+int dfsn,dfsroot,child,n;
+int ans,nc,tot;
+stack<int> Q;
+void dfs(int u)
+{
+    tot++;
+    dfsnum[u]=dfslo[u]=dfsn++;
+    Q.push(u);
+    int z=E[u].size();
+    for(int i=0;i<z;i++)
+    {
+        int v=E[u][i];
+        if(dfsnum[v]==0)
+        {
+            par[v]=u;
+            if(u==dfsroot)
+                    child++;
+            dfs(v);
+            if(dfslo[v]>=dfsnum[u]){
+             if(artp[u]==false) {realArt.pb(u);artn[u]=ans;ans++;}
+             artp[u]=true;
+             C.pb(vi());
+             C[C.size()-1].pb(u);
+             while(1)
+             {
+              int temp=Q.top();
+              C[C.size()-1].pb(temp);
+              Q.pop();
+              if(temp==v) break;
+             }
+            }
+            dfslo[u]=min(dfslo[u],dfslo[v]);
+        }
+        else if(v != par[u])
+            dfslo[u]=min(dfslo[u],dfsnum[v]);
+    }
+    return ;
+}
+
+int main()
+{
+    int T,I=1;
+    cin>>T;
+    while(T--){
+    printf("Case %d:\n",I++);
+    ans=0;
+    scanf("%d",&n);
+    E.assign(n+1,vi());
+    int m,a,b;
+    scanf("%d",&m);
+    for(int i=0;i<m;i++)
+    {
+        scanf("%d %d",&a,&b);
+        E[a].push_back(b);
+        if(a!=b)
+        E[b].push_back(a);
+    }
+    // Bi-Connected Component
+    memset(dfsnum,0,sizeof(dfsnum));
+    memset(artp,false,sizeof(artp));
+    for(int i=1;i<=n;i++)
+        if(dfsnum[i]==0)
+    {
+        tot=0;
+        dfsn=1;
+        ans=0;
+        while(!Q.empty()) Q.pop();
+        C.clear();
+        realArt.clear();
+        dfsroot=i;
+        child=0;
+        dfs(i);
+        artp[i]= (child > 1);
+        // Block Cut Tree
+        nc=C.size();
+        if(nc==0)
+        {
+          Ans[i]=0;
+          continue;
+        }
+       //cout<<nc<<" "<<" "<<tot<<endl;
+        G.assign(nc+ans+1,vi());
+        rep2(j,nc)
+          rep2(k,SZ(C[j]))
+            if(artp[C[j][k]])
+              {
+                G[ans+j].pb(artn[C[j][k]]);
+                G[artn[C[j][k]]].pb(ans+j);
+              }
+            else Ans[C[j][k]]=tot-1;
+       // tree_dp(0,-1);
+    }
+    // rep2(i,SZ(C)){
+    //   rep2(j,SZ(C[i]))
+    //   cout<<C[i][j]<<" ";
+    //   cout<<endl;
+    // }
+    rep1(i,n) printf("%lld\n",Ans[i]);
+  }
+  return 0;
+}
+ 
